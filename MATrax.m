4654167b2cdb13007c8c1c53ed06f9d7d1a7268c
Kevin Gao <sudowork@gmail.com>
2013-04-17 02:41:14 -0400

    Separated engine from gui. Added dummy load library interaction

--- MATrax.m
+++ MATrax.m
@@ -1,53 +1,72 @@
 classdef MATrax < handle
 % MATRAX Singleton for running MATrax application
-%        A GUI-based DJ software, built in MATLAB.
+%        A GUI-based DJ software, built in MATLAB. Acts as a front-end interface
+%        for theh MATRAXENGINE.
 %
 % Author: Kevin Gao
 % Usage: call `MATrax` to start application and initialize the GUI
-% See Also: MATRAXGUI
+% See Also: MATRAXENGINE
+  %% Properties
   properties (Constant)
     GUI_NAME = 'MATrax';
     GUI_WIDTH = 800;
     GUI_HEIGHT = 600;
-  end % constants
+  end
 
   properties (Access=private)
-    f   % figure containing GUI
-  end % private instance variables
+    f       % figure containing GUI
+    eng     % engine for actually processing music
+  end
 
   methods (Access=private)
+    %% GUI initialization methods
     function initGUI(this)
       this.f = figure(...
                    'Name', MATrax.GUI_NAME,...
                    'MenuBar', 'none',...
                    'Toolbar', 'none',...
-                   'Position', [0,0,MATrax.GUI_WIDTH,MATrax.GUI_HEIGHT],...
+                   'Position', [0 0 MATrax.GUI_WIDTH MATrax.GUI_HEIGHT],...
                    'Visible', 'off');
       movegui(this.f, 'center');
-    end % initGUI
+    end
+
+    function addMenus(this)
+      e = this.eng;
+      mFile = uimenu(this.f, 'Label', 'File');
+      uimenu(mFile, 'Label', 'Load Library',...
+                    'Accelerator', 'L',...
+                    'Callback', {@(src, event) e.loadLibrary()});
+    end
 
     function addComponents(this)
-    end % addComponents
+    end
 
     function initComponents(this)
-    end % initComponents
+    end
 
     function setupCallbacks(this)
-    end % setupCallbacks
+    end
 
     function displayGUI(this)
       set(this.f, 'Visible', 'on');
+      disp('MATrax GUI Initialized');
       uiwait(this.f);
-    end % displayGUI
-  end % private methods
+    end
+
+  end
 
   methods (Static)
+    %% MATrax Constructor
     function obj = MATrax()
+      % Create new MATraxEngine
+      obj.eng = MATraxEngine(obj);
+      % Set up GUI
       initGUI(obj);
+      addMenus(obj);
       addComponents(obj);
       initComponents(obj);
       setupCallbacks(obj);
       displayGUI(obj);
-    end % MATrax
+    end
   end
 end
---
+++ MATraxEngine.m
@@ -0,0 +1,26 @@
+classdef MATraxEngine < handle
+% MATRAXENGINE Engine for MATRAX DJ platform
+%              Maintains state for the MATRAX application and acts as the
+%              overarching music composer.
+%
+% Author: Kevin Gao
+% Usage: call `MATrax` to start application and initialize the GUI
+% See Also: MATRAX
+  properties (Access=private)
+    gui     % reference to GUI front-end
+    libDir  % path to music library
+  end
+
+  methods
+    %% MATraxEngine Constructor
+    function this = MATraxEngine(gui)
+      this.gui = gui;
+    end
+
+    %% Callback methods
+    function loadLibrary(this)
+      this.libDir = uigetdir(pwd(), 'Open Directory Containing Tracks');
+      fprintf('Library loaded: %s\n', this.libDir);
+    end
+  end
+end
